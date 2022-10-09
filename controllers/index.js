7c6b80a1243c5fbc1dcd0fc4cf67ba97171b43ae
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-10-09 16:45:22 -0400

    created index.js in api folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+router.use('/user', userRoutes);
+router.use('/post', postRoutes);
+
+// const apiRoutes = require('./api');
+// const homeRoutes = require('./home-routes.js');
+
+// router.use('/', homeRoutes);
+// router.use('/api', apiRoutes);
 
 module.exports = router;
