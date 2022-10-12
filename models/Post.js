d438d2092c25c7e2a7be3c1500eb9b1653ba90e2
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-10-12 14:47:34 -0400

    updated Post.js in models

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {}
+class Post extends Model { }
 
 Post.init(
     {
@@ -11,16 +11,25 @@ Post.init(
             primaryKey: true,
             autoIncrement: true,
         },
-        post: {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
             type: DataTypes.STRING,
         },
-        // user_id: {
-        //     type: DataTypes.INTEGER,
-        //     references: {
-        //         model: 'user',
-        //         key: 'id',
-        //     },
-        // },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
     },
     {
         sequelize,
@@ -28,7 +37,38 @@ Post.init(
         freezeTableName: true,
         underscored: true,
         modelName: 'post',
-    },
+    }
 );
 
 module.exports = Post;
+
+
+// Post.init(
+//     {
+//         id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             primaryKey: true,
+//             autoIncrement: true,
+//         },
+//         post: {
+//             type: DataTypes.STRING,
+//         },
+//         user_id: {
+//             type: DataTypes.INTEGER,
+//             references: {
+//                 model: 'user',
+//                 key: 'id',
+//             },
+//         },
+//     },
+//     {
+//         sequelize,
+//         timestamps: false,
+//         freezeTableName: true,
+//         underscored: true,
+//         modelName: 'post',
+//     },
+// );
+
+// module.exports = Post;
