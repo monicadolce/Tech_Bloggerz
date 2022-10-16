6c650d44eaf8fc6bb32a62e19203a84a1a7e32e2
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-10-16 17:27:07 -0400

    added comments to index.js in models

--- models/index.js
+++ models/index.js
@@ -1,28 +1,35 @@
+// import models
 const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+// Users have many Posts 
 User.hasMany(Post, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE',
 });
 
+// Post belongs to User
 Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
+// Posts have many Comments
 Post.hasMany(Comment, {
     foreignKey: 'post_id',
 });
 
+// Comment belongs to Post
 Comment.belongsTo(Post, {
     foreignKey: 'post_id',
 })
 
+// Comment belongs to User
 Comment.belongsTo(User, {
     foreignKey: 'user_id'
 })
 
+// Users have many Comments
 User.hasMany(Comment, {
     foreignKey: 'user_id',
 });
