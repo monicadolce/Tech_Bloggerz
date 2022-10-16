9da86b57839d3066dc4da336c5f6384e03184df3
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-10-16 17:58:12 -0400

    added comment to logout.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,4 +1,5 @@
 const logout = async () => {
+  // Send a POST request to the API endpoint
   const response = await fetch('/api/users/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
