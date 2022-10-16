fc8ed96d517e7661053bd9664eb5c25d73ddb074
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-10-16 16:03:43 -0400

    removed commented out sections from homeRoutes.js, userRoutes.js, postRoutes.js, commentRoutes.js

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -37,19 +37,7 @@ router.delete('/:id', withAuth, async (req, res) => {
 
 module.exports = router;
 
-// Post route allows to create a post with text
-// router.post('/', withAuth, async (req, res) => {
-//     try {
-//       const postData = await Post.create({
-//         post: req.body.post,
-//         user_id: req.session.user_id,
-//       });
-//       console.log(postData)
-//       res.status(200).json(postData);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
+
   // Put route allows to update the post based on the user id
   router.put('/:id', withAuth, (req, res) => {
     Post.update(
@@ -71,22 +59,3 @@ module.exports = router;
       });
   });
   
-//   // Delete route allows to delete the post based on the user id
-//   router.delete('/:id', withAuth, async (req, res) => {
-//     try {
-//       const postData = await Post.destroy({
-//         where: {
-//           id: req.params.id,
-//         },
-//       });
-//       if (!postData) {
-//         res.status(404).json({ message: "No post found with this id!" });
-//         return;
-//       }
-//       res.status(200).json(postData);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
-  
-//   module.exports = router;
