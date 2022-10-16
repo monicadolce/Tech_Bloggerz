efa8218690fd28e225b7cc5800394428d6a6a2a5
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-10-16 17:56:17 -0400

    added comments to editPost.js

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,21 +1,26 @@
 const savePost = async (event) => {
     event.preventDefault();
+
+    // Collect values from the login form
     const name = document.getElementById('title').value
     const description = document.getElementById('description').value
-   
-        const id =  window.location.toString().split('/')[window.location.toString().split('/').length - 1];
 
-        const response = await fetch(`/api/posts/${id}`, {
-            method: 'PUT',
-            body: JSON.stringify({name, description}),
-            headers: {'Content-Type': 'application/json'}
-            
-        });
-        if (response.ok) {
-            document.location.replace(`/post/${id}`)
-        } else {
-            alert('Failed to edit post')
-        }
+    // Grabs id by splitting window location URL and returns an array of 
+    // strings minus 1 in order to get the last element of the array which corresponds to id 
+    const id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
+
+    // Send a PUT request to the API endpoint
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ name, description }),
+        headers: { 'Content-Type': 'application/json' }
+
+    });
+    if (response.ok) {
+        document.location.replace(`/post/${id}`)
+    } else {
+        alert('Failed to edit post')
+    }
 };
 
 document.querySelector('.edit-post-form').addEventListener('submit', savePost);
