84659eb618535520b5c2704a4c9ef851d822bf68
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-10-16 17:35:04 -0400

    added comment to commentRoutes.js

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
-
+// Create a comment
 router.post('/', async (req, res) => {
     try {
         const newComment = await Comment.create({
